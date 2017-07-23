c00ea97ecbfba82a6fdd580ac1b88a4df3154bc2
Aikis07 <danila.barkov@gmail.com>
2017-07-23 17:32:07 +0300

    Calculate checkstyle

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,18 @@
+﻿/**
+* Package for calculate task.
+*
+* @author Danila Barkov (mailto:danila.barkov@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
 package ru.job4j;
 
-public class Calculate{
-	public static void main(String[] args){
+public class Calculate {
+	/**
+	* Main.
+	* @param args - args
+	*/
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 }
