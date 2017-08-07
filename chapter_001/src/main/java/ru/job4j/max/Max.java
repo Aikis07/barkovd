552b4c6067d4684ab250e2f3f80076a5d7bb6185
Aikis07 <danila.barkov@gmail.com>
2017-08-07 23:32:11 +0300

    Task_190

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -18,4 +18,13 @@ public class Max {
         int result = first > second ? first : second;
         return result;
     }
+    /**.
+     * Метод выполняет сравнение трех переменных.
+     * @param first - first
+     * @param second - second
+     * @param third - third
+     */
+    public int max(int first, int second, int third) {
+        return max(max(first, second), third);
+    }
 }
