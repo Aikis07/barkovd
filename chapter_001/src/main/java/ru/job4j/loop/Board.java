162e7a86320d122bd30771c7b6d6d4ee26619cee
Aikis07 <danila.barkov@gmail.com>
2017-08-13 17:56:19 +0300

    Task_13559

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Board {
+    public String paint(int width, int height){
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i <= height; i++){
+            for (int j = 1; j <= width; j++){
+                if ((i + j) % 2 == 0){
+                    builder.append("x");
+                } else {
+                    builder.append(" ");
+                }
+            }
+            builder.append(System.getProperty("line.separator"));
+        }
+        String completedString = builder.toString();
+        return completedString;
+    }
+}
